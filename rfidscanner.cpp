56e7d74472cba7a50d8aa39893fc099705ee41d2
Henry <henrybulbeck@gmail.com>
2026-01-21 15:01:20 +1100

    added RFID scanner model (friis fspl) to scanner plugin

--- rfidscanner.cpp
+++ rfidscanner.cpp
@@ -11,6 +11,7 @@
 #include <gz/plugin/Register.hh>
 
 #include <string>
+#include <cmath>
 
 GZ_ADD_PLUGIN(RFIDScannerPlugin,
 		gz::sim::System,
@@ -18,8 +19,15 @@ GZ_ADD_PLUGIN(RFIDScannerPlugin,
 		RFIDScannerPlugin::ISystemPreUpdate
 )
 
-RFIDScannerPlugin::RFIDScannerPlugin(void)
+RFIDScannerPlugin::RFIDScannerPlugin(void) :
+	gen(rd()),
+	distribution(0,1)
 {
+}
+
+double RFIDScannerPlugin::sigmoid(double x)
+{
+	return 1.0 / (1.0 + std::exp(-x));
 }
 
 bool RFIDScannerPlugin::scanRequestCallback(gz::custom_msgs::RFIDScanResponse& _reply)
@@ -49,12 +57,15 @@ bool RFIDScannerPlugin::scanRequestCallback(gz::custom_msgs::RFIDScanResponse& _
 	// Retrieve current pose of the scanner
 	auto sp = scanner_link.WorldPose(*ecm_internal);
 
+	// Retrieve scanner orientation
+	gz::math::Quaterniond& scanner_orientation = sp->Rot();
+
 	/* Here is where we iterate over all the tags in the simulation and check to see whether they meet the criteria
 	 * to be scanned.
 	 *
 	 * Our RFIDScanResponse message has the following format:
 	 *
-	 *
+	 * TODO
 	 *
 	 *
 	 */
@@ -71,18 +82,63 @@ bool RFIDScannerPlugin::scanRequestCallback(gz::custom_msgs::RFIDScanResponse& _
 			// Retrieve pose of tag
 			auto wp = link.WorldPose(*ecm_internal);
 
+			// TODO Need to make this far more advanced (as per RFID model)
+			/* Here, we determine if the tag should be scanned as per our RFID model. We have a number of
+			 * parameters that are retrieved from the SDF during the Configure method.
+			 *
+			 * In particular, we are interested in the relative orientation between the tag and the scanner,
+			 * as well as the linear distance between the two.
+			 * 
+			 * See the blog post at TODO for the full derivation of the equations but we need to calculate
+			 *
+			 * TODO Add in the antenna (directional) gain term (Gt)
+			 * TODO 
+			 *
+			 */
 			if (wp && sp) {
-				//gz::math::Pose3d model_pose = *wp;
-				gzwarn << model.Name(*ecm_internal) << "tag pose(" << *wp << ") scanner pose(" << *sp << ")\n";
-
 				// Calculate linear distance
-				//gzwarn << "Linear distance: " << (wp->CoordPositionSub(*sp)).Length() << "\n";
-				
 				double tag_scanner_linear_distance = (wp->CoordPositionSub(*sp)).Length();
 
-				// TODO Need to make this far more advanced (as per RFID model)
-				if (tag_scanner_linear_distance < 100.0) {
-					// Add tag to list of found tags
+				// Calculate dot product between scanner and tag
+				gz::math::Quaterniond& tag_orientation = wp->Rot();
+				double cos_theta = tag_orientation.Dot(scanner_orientation);
+
+				// Calculate tranmission power
+				
+				// TODO This is wrong - the max should be applied before(?) the cosine
+				double loss_polarization = std::min(
+						polarization_max_loss,
+						-20*std::log10(
+							std::max(polarization_minimum_angle, std::abs(cos_theta))
+						)
+					);
+
+				// TODO Replace with the check for LOS / NLOS between reader and tag
+				double loss_path;
+				if (true) {
+					loss_path = 10*path_loss_los_gain*std::log10(tag_scanner_linear_distance / path_loss_scaling);
+				} else {
+					loss_path = 10*path_loss_nlos_gain*std::log10(tag_scanner_linear_distance / path_loss_scaling);
+				}
+
+				double transmission_power = std::min(100.0,
+						std::max(-20.0, antenna_gain + antenna_directional_gain + tag_directional_gain - loss_polarization - loss_path)
+						);
+
+				double received_power = std::min(100.0,
+						std::max(-20.0, antenna_gain + antenna_directional_gain + tag_directional_gain - 2*(loss_polarization + loss_path)));
+
+				// Calculate read probabilities
+				
+				double p_read_tx = sigmoid((transmission_power - tx_threshold_power) / tx_read_scaling);
+				double p_read_rx = sigmoid((received_power - rx_threshold_power) / rx_read_scaling);
+
+				double p_read = p_read_tx*p_read_rx;
+
+				gzwarn << model.Name(*ecm_internal) << " " << *wp << " , power: " << transmission_power << ", p(read): " << p_read << "\n";
+
+				if ( distribution(gen) < p_read ) {
+					// Successful read - add tag to list of found tags
 
 					auto* scanmessage = _reply.add_scan();
 
--- rfidscanner.h
+++ rfidscanner.h
@@ -6,6 +6,7 @@
 #include <gz/transport/Node.hh>
 
 #include <chrono>
+#include <random>
 
 #include <gz/custom_msgs/rfid_scan_response.pb.h>
 
@@ -85,9 +86,50 @@ class RFIDScannerPlugin :
 
 	private:
 		// Configuration parameters (set via SDF)
-		
-		/* @brief Frequency that we update our scanned tag list */
-		uint16_t scan_period{10};
 
+		/* @brief The path loss factor when we have line of sight to the tag */
+		double path_loss_los_gain{2.2};
+
+		/* @brief The path loss factor when we do not have lin of sight to the tag */
+		double path_loss_nlos_gain{3};
+
+		/* @brief Path loss scaling factor */
+		double path_loss_scaling{5};
+
+		/* @brief The minimum cos(\theta) value that we should have (avoids excessive loss from polarization) */
+		double polarization_minimum_angle{0.05};
+
+		/* @brief Maximum loss from polarization angle (in dB) */
+		double polarization_max_loss{25};
+
+		/* @brief Antenna transmission gain (in dB) */
+		double antenna_gain{30};
+
+		// TODO This will become dependent on the direction of the antenna
+		/* @brief Antenna (directional) gain */
+		double antenna_directional_gain{6};
+
+		/* @brief Tag (directional) gain */
+		double tag_directional_gain{0};
+
+		/* @brief Power threshold at which transmit and received waves each have 50% read probability (for sigmoid) */
+		double tx_threshold_power{30};
+		double rx_threshold_power{30};
+
+		/* @brief Scaling parameter for rx and tx read probabilities */
+		double tx_read_scaling{1};
+		double rx_read_scaling{1};
+
+		// TODO Add LOS static terms
+	
+	private:
+		/* @brief Sigmoid function implementation */
+		double sigmoid(double x);
+
+		std::random_device rd;
+
+		std::mt19937 gen;
+
+		std::uniform_real_distribution<> distribution;
 
 };
