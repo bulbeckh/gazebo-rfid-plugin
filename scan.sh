4f0cad19c5320c87e7241682757e6fea136b5ca7
Henry <henrybulbeck@gmail.com>
2026-03-06 11:49:57 +1100

    added timout to scan service call. For environemnt w ~500tags, takes a few seconds to conduct full scan so timeout param is required

--- scan.sh
+++ scan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gz service -s /scan_request --reqtype gz.msgs.Empty --reptype gz.custom_msgs.RFIDScanResponse -r ''
+gz service --timeout 10000 -s /scan_request --reqtype gz.msgs.Empty --reptype gz.custom_msgs.RFIDScanResponse -r ''
