69072fbc0dd07cb620a920ec6e53a068a27a002d
Henry <henrybulbeck@gmail.com>
2026-03-20 21:20:27 +1100

    added ros2 interface for package - package.xml, env hooks, and ament_cmake to the cmakelists.txt

--- create.sh
+++ create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gz service -s /rfid_tag_create --reqtype gz.custom_msgs.RFIDTagList --reptype gz.msgs.Boolean\
+gz service --timeout 10000 -s /rfid_tag_create --reqtype gz.custom_msgs.RFIDTagList --reptype gz.msgs.Boolean\
 	-r 'tags:
 		[{
 			uid: "x22434",
